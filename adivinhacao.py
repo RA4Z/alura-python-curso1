fd6f8c5e4600d6f4079c160535039bc1d2f1ae1f
RA4Z <rraz639@gmail.com>
2023-06-09 13:49:21 -0300

    Adicionando condições no projeto

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,15 @@
+print('*********************************')
 print('Bem vindo ao jogo de Adivinhação!')
+print('*********************************')
+
+numero_secreto = 42
+
+chute = input('Digite o seu número: ')
+chute = int(chute)
+
+print('Você digitou ', chute)
+
+if (chute == numero_secreto):
+    print('Você acertou')
+else:
+    print('Você errou')
