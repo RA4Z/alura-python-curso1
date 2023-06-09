0e7c67ee49af83556885f620529daae0d9a59453
RA4Z <rraz639@gmail.com>
2023-06-09 14:00:02 -0300

    Adicionando condições para verificar se acertou ou errou o número secreto

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,19 @@ numero_secreto = 42
 chute = input('Digite o seu número: ')
 chute = int(chute)
 
+acertou     = chute == numero_secreto
+maior       = chute > numero_secreto
+menor       = chute < numero_secreto
+
 print('Você digitou ', chute)
 
-if (chute == numero_secreto):
+if (acertou):
     print('Você acertou')
 else:
-    print('Você errou')
+    if(maior):
+        print('Você errou! O seu chute foi maior do que o número secreto!')
+    elif(menor):
+        print('Você errou! O seu chute foi menor do que o número secreto!')
+
+
+print('Fim do jogo!')
