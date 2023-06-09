86c3c03361418504567ff9719cb35caf7561a850
RA4Z <rraz639@gmail.com>
2023-06-09 15:13:49 -0300

    Usando continue e break no projeto

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,17 +7,21 @@ total_de_tentativas = 3
 
 for rodada in range(1,total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
-    chute = input('Digite o seu número: ')
+    chute = input('Digite um número entre 1 e 100: ')
     chute = int(chute)
     print('Você digitou ', chute)
+
+    if(chute < 1 or chute > 100):
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
 
     acertou     = chute == numero_secreto
     maior       = chute > numero_secreto
     menor       = chute < numero_secreto
 
     if (acertou):
-        print('Você acertou')
-        total_de_tentativas = 0
+        print('Você acertou!')
+        break
     else:
         if(maior):
             print('Você errou! O seu chute foi maior do que o número secreto!')
