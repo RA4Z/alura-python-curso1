34a0792c06a7c519f156fd010bf8b0e6b90a226c
RA4Z <rraz639@gmail.com>
2023-06-09 14:17:39 -0300

    Colocando o código dentro de um loop while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,23 +3,28 @@ print('Bem vindo ao jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input('Digite o seu número: ')
-chute = int(chute)
+while (rodada <= total_de_tentativas):
+    print('Tentativa', rodada, 'de', total_de_tentativas)
+    chute = input('Digite o seu número: ')
+    chute = int(chute)
+    print('Você digitou ', chute)
 
-acertou     = chute == numero_secreto
-maior       = chute > numero_secreto
-menor       = chute < numero_secreto
+    acertou     = chute == numero_secreto
+    maior       = chute > numero_secreto
+    menor       = chute < numero_secreto
 
-print('Você digitou ', chute)
+    if (acertou):
+        print('Você acertou')
+        total_de_tentativas = 0
+    else:
+        if(maior):
+            print('Você errou! O seu chute foi maior do que o número secreto!')
+        elif(menor):
+            print('Você errou! O seu chute foi menor do que o número secreto!')
 
-if (acertou):
-    print('Você acertou')
-else:
-    if(maior):
-        print('Você errou! O seu chute foi maior do que o número secreto!')
-    elif(menor):
-        print('Você errou! O seu chute foi menor do que o número secreto!')
-
+    rodada = rodada + 1
 
 print('Fim do jogo!')
