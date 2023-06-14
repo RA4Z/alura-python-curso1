820e9790c26ce23daeabef4086058a592fdb209f
RA4Z <rraz639@gmail.com>
2023-06-14 15:49:13 -0300

    Procurando pela letra inserida em cada index da palavra

--- forca.py
+++ forca.py
@@ -10,6 +10,15 @@ def jogar():
 
     #Enquanto não enforcou e não acertou
     while(not enforcou and not acertou):
+
+        chute = input('Qual letra? ')
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print('Encontrado a letra {} na posição {}'.format(letra,index))
+            index = index + 1
+
         print('jogando ...')
 
     print('Fim de jogo')
