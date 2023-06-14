8243a0960ec8f5a5d881b5d1bcec0a28140e0b94
RA4Z <rraz639@gmail.com>
2023-06-14 16:00:14 -0300

    Tratando as strings com strip e upper

--- forca.py
+++ forca.py
@@ -12,11 +12,12 @@ def jogar():
     while(not enforcou and not acertou):
 
         chute = input('Qual letra? ')
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
-                print('Encontrado a letra {} na posição {}'.format(letra,index))
+            if(chute.upper() == letra.upper()):
+                print('Encontrado a letra {} na posição {}'.format(chute.upper(),index))
             index = index + 1
 
         print('jogando ...')
