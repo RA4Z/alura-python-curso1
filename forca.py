e751fe8562bca2a26cb0b60837d9b25e5e883905
RA4Z <rraz639@gmail.com>
2023-06-14 15:39:49 -0300

    Fazendo o loop do projeto

--- forca.py
+++ forca.py
@@ -4,6 +4,14 @@ def jogar():
     print('***Bem vindo ao jogo da Forca!***')
     print('*********************************')
 
+    palavra_secreta = 'palavra'
+    enforcou = False
+    acertou = False
+
+    #Enquanto não enforcou e não acertou
+    while(not enforcou and not acertou):
+        print('jogando ...')
+
     print('Fim de jogo')
 
 if(__name__ == '__main__'):
